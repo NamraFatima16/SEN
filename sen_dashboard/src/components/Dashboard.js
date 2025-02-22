9ed662102a11594049b19bcbc3f40229b8ee97d0
Namra Fatima <20094326@mail.wit.ie>
2025-02-22 08:50:59 +0000

    Hello world the dashboard

---
+++ sen_dashboard/src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class Dashboard extends Component {
+
+    render(){
+
+        return (
+
+            <p>Hello World Dashboard</p>
+        )
+
+
+    }
+
+
+
+
+}
